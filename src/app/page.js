e417e965fb4632ca4ae69be04c16ff9a3e7b93b2
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2026-06-11 11:36:27 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
- export default function Home() {
+ export default function Landing() {
   return (
-<div>
-  Hii
-</div>
+<main>
+  <section>
+    <h1 className = "text-muted text-3xl font-bold tracking-tighter sm;text-4xl md:text-5xl lg:text-6xl">Manage your content with Ease</h1>
+  </section>
+  <section></section>
+  <section></section>
+</main>
   );
 }
